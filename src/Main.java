a7c9dc5c4fdf10bcef7311406ad8a3c24ea5c344
Bruno Šarlija <bruno.sarlija@gmail.com>
2023-01-20 18:29:00 +0100

    comit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+/*Korisnik pogađa vrijednost broja definiranog u programu (može biti predefiniran, kao
+        konstanta). Računalo mu vraća poruke veći/manji dok god ga ne pogodi. Kada ga pogodi,
+        ispiše mu broj pokušaja (npr. “Bravo! Pogodio/la si broj zadan u programu, trebalo ti je 7
+        pokušaja”).*/
+public class Main {
+
+
+}
